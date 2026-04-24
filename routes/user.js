7ccc2560c3605cf56581a7744acfeaeec7fa48bb
Saurav Jorwal <sauravjorwal44@gmail.com>
2026-04-24 18:30:24 +0530

    updated project

--- routes/user.js
+++ routes/user.js
@@ -4,15 +4,29 @@ const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
-const { signup } = require("../controllers/users.js");
 const userController = require("../controllers/users.js");
 
-router.route("/signup")
-    .get(userController.renderSignupForm)
-    .post(wrapAsync(userController.signup))
-router.route("/login")
-    .get( userController.renderLoginForm )
-    .post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
-    
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
+
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login,
+  );
+
 router.get("/logout", userController.logout);
-module.exports = router; 
+
+router.get("/travel-map", (req, res) => {
+  res.render("listings/travel-map");
+});
+
+module.exports = router;
